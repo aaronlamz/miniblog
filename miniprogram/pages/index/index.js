d3b535c423b106e9612dd7a5e03e1e4588e8a5c1
linjiajun <qylinxia@qq.com>
2018-09-29 21:41:27 +0800

    autoplay

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -9,7 +9,7 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
     indicatorDots: true,
-    autoplay: false,
+    autoplay: true,
     circular: true,
     interval: 5000,
     duration: 1000,
