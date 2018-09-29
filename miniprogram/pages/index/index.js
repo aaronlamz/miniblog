adea66b9ebffd9b0ff65080eaa0aed1aeb39fbad
linjiajun <qylinxia@qq.com>
2018-09-29 17:47:46 +0800

    添加页面跳转

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -20,8 +20,8 @@ Page({
       title: 'Webpack揭秘——走向高阶前端的必经之路',
       imgUrl: '../../images/list/list_02.png'
     }, {
-      title: '【开源公告】H5、小程序自动化测试框架FAutoTest正式开源',
-      imgUrl: '../../images/list/list_01.png'
+      title: '还在记git命令？快试试SourceTree',
+      imgUrl: '../../images/list/list_03.png'
     }, {
       title: 'Webpack揭秘——走向高阶前端的必经之路',
       imgUrl: '../../images/list/list_02.png'
@@ -52,12 +52,19 @@ Page({
 
   onPullDownRefresh: function() {
     setTimeout(function() {
-      wx.stopPullDownRefresh();
-    }, 300);
+      wx.showToast({
+        title: '没有更多数据',
+        success: function() {
+          wx.stopPullDownRefresh();
+        }
+      });
+    }, 500);
   },
 
   onReachBottom: function() {
-    wx.showLoading();
+    wx.showLoading({
+      title: '玩命加载中'
+    });
     setTimeout(() => {
       wx.hideLoading();
       if (this.data.list.length > 10) {
@@ -71,6 +78,9 @@ Page({
         }, {
           title: 'Webpack揭秘——走向高阶前端的必经之路',
           imgUrl: '../../images/list/list_02.png'
+        }, {
+          title: '还在记git命令？快试试SourceTree',
+          imgUrl: '../../images/list/list_03.png'
         });
         this.setData({
           list: this.data.list
@@ -78,9 +88,10 @@ Page({
       }
     }, 300);
   },
+
   goDetail: function() {
-    wx.showToast({
-      title: '功能开发中'
+    wx.navigateTo({
+      url: '../userConsole/userConsole'
     });
   }
 })
