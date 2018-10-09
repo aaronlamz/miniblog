0c0326a7ed7e374e89c66203a9f03d7a9ca6dc82
linjiajun <qylinxia@qq.com>
2018-10-09 17:35:17 +0800

    add authority

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -3,21 +3,32 @@ const app = getApp()
 const db = app.db
 
 Page({
-  data: {},
+  data: {
+    isAutority: false,
+    avatarUrl: '',
+    nickName: ''
+  },
 
   onLoad: function() {
-    this.getArticleList();
-  },
-
-  onPullDownRefresh: function() {
-
-  },
-
-  onReachBottom: function() {},
-
-  getArticleList: function() {
-    db.collection('article_conetnt').get().then(res => {
-      console.log(res.data);
+    // 查看是否授权
+    wx.getSetting({
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          this.setData({
+            isAutority: true
+          })
+          wx.getUserInfo({
+            success: function(res) {
+              console.log(res)
+            }
+          })
+        }
+      }
     })
+  },
+
+  bindGetUserInfo(e) {
+    console.log(e.detail.userInfo)
   }
 })
